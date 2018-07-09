67497f918d2356c0ba67eb0b6b25f856f7e4d2c1
Michael Boroski <mjboroski@gmail.com>
2018-07-09 08:11:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,11 +18,11 @@ class BoardMember{
   }
 
   releasePressStatement(){
-    return "You will see great things from Scuber.";
+    return `You will see great things from Scuber.`;
   }
 
   sayHi(){
-    return 'Hi, my name is ${this.name}. I am from ${this.homestate}, and I was trained in ${this.training}.';
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
 }
 
